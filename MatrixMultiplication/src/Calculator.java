e8188b4daa3e1e2cb19d1d7c58f1f7ee1a69ab93
kstimson <kennyman94@aol.com>
2014-01-28 20:41:48 -0300

    Added and commented the Calculator class.

---
+++ MatrixMultiplication/src/Calculator.java
@@ -0,0 +1,62 @@
+
+public class Calculator {
+	
+	/**
+	 * get the rowth, colth minor of the specified matrix.
+	 * @param matrix 
+	 * @param row 
+	 * @param col 
+	 * @return
+	 */
+	public int minor(int[][] matrix, int row, int col){
+		return 0;
+	}
+	
+	/**
+	 * recurse with n-1xn-1 matrix
+	 * base case: 2 by 2 matrix
+	 * @param matrix the matrix to get the determinant of
+	 * @return
+	 */
+	public int determinant(int[][] matrix){
+		return 0;
+	}
+	
+	/**
+	 * Scalar multiplication of a matrix
+	 * @param matrix
+	 * @param scalar
+	 * @return
+	 */
+	public int multiply(int[][] matrix, int scalar){
+		return 0;
+	}
+	
+	/**
+	 * multiplies two matrices together
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int multiply(int[][] a, int[][] b){
+		return 0;
+	}
+	
+	/**
+	 * transposes the input matrix such that the rows become the cols and vice versa
+	 * @param a
+	 * @return
+	 */
+	public int[][] transpose(int[][] a){
+		return null;
+	}
+	
+	/**
+	 * calculates the adjoint of a
+	 * @param a
+	 * @return
+	 */
+	public int[][] adjoint(int[][] a){
+		return null;
+	}
+}
