b6d4ce72d6aeefd6f7cd570d097cbcba74747696
kstimson <kennyman94@aol.com>
2014-01-29 17:57:51 -0300

    Implemented all of the Calculator methods. Need to test them still.

--- MatrixMultiplication/src/Calculator.java
+++ MatrixMultiplication/src/Calculator.java
@@ -8,8 +8,14 @@ public class Calculator {
 	 * @param col 
 	 * @return
 	 */
-	public int minor(int[][] matrix, int row, int col){
-		return 0;
+	public int[][] minor(int[][] matrix, int row, int col){
+		//preconditions
+		if(matrix == null)throw new IllegalArgumentException("matrix must not be null");
+		for(int i = 0; i < matrix.length; i++){
+			if(matrix.length != matrix[i].length)throw new IllegalArgumentException("matrix must be square!");
+		}
+		
+		return removeRowAndCol(matrix, row, col);
 	}
 	
 	/**
@@ -19,27 +25,77 @@ public class Calculator {
 	 * @return
 	 */
 	public int determinant(int[][] matrix){
-		return 0;
+		//preconditions
+		if(matrix == null)throw new IllegalArgumentException("matrix must not be null");
+		for(int i = 0; i < matrix.length; i++){
+			if(matrix.length != matrix[i].length)throw new IllegalArgumentException("matrix must be square!");
+		}
+		
+		if(matrix.length == 2){
+			return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+		}
+		
+		int sign = 1;
+		int total = 0;
+		for(int i = 0; i < matrix[0].length; i++){
+			total += sign * matrix[0][i] * determinant(removeRowAndCol(matrix, 0, i));
+			sign*= -1;
+		}
+		
+		return total;
 	}
 	
 	/**
-	 * Scalar multiplication of a matrix
+	 * Scalar multiplication of a matrix. Works by mutating the array that is passed in.
 	 * @param matrix
 	 * @param scalar
 	 * @return
 	 */
-	public int multiply(int[][] matrix, int scalar){
-		return 0;
+	public void multiply(int[][] matrix, int scalar){
+		//preconditions
+		if(matrix == null)throw new IllegalArgumentException("matrix must not be null");
+		for(int i = 0; i < matrix.length; i++){
+			if(matrix.length != matrix[i].length)throw new IllegalArgumentException("matrix must be square!");
+		}
+		
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix.length; j++){
+				matrix[i][j]*= scalar;
+			}
+		}
 	}
 	
 	/**
 	 * multiplies two matrices together
 	 * @param a
 	 * @param b
-	 * @return
+	 * @return a multiplied by b
 	 */
-	public int multiply(int[][] a, int[][] b){
-		return 0;
+	public int[][] multiply(int[][] a, int[][] b){
+		//preconditions
+		if(a == null)throw new IllegalArgumentException("a must not be null");
+		if(b == null)throw new IllegalArgumentException("b must not be null");
+		for(int i = 0; i < a.length; i++){
+			if(a.length != a[i].length)throw new IllegalArgumentException("a must be square!");
+		}
+		
+		for(int i = 0; i < b.length; i++){
+			if(b.length != b[i].length)throw new IllegalArgumentException("b must be square!");
+		}
+		
+		if(a.length != b.length)throw new IllegalArgumentException("a and b must be the same size");
+		
+		int[][] newMatrix = new int[a.length][a.length];
+		
+		for(int i = 0; i < a.length; i++){
+			for(int j = 0; j < a.length; j++){
+				for(int k = 0; k < a.length; k++){
+					newMatrix[i][j] += a[i][k]*b[k][j];
+				}
+			}
+		}
+		
+		return newMatrix;
 	}
 	
 	/**
@@ -47,8 +103,21 @@ public class Calculator {
 	 * @param a
 	 * @return
 	 */
-	public int[][] transpose(int[][] a){
-		return null;
+	public int[][] transpose(int[][] matrix){
+		//preconditions
+		if(matrix == null)throw new IllegalArgumentException("matrix must not be null");
+		for(int i = 0; i < matrix.length; i++){
+			if(matrix.length != matrix[i].length)throw new IllegalArgumentException("matrix must be square!");
+		}
+		
+		int[][] newMatrix = new int[matrix.length][matrix.length];
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix.length; j++){
+				newMatrix[j][i] = matrix[i][j];
+			}
+		}
+		
+		return newMatrix;
 	}
 	
 	/**
@@ -56,7 +125,45 @@ public class Calculator {
 	 * @param a
 	 * @return
 	 */
-	public int[][] adjoint(int[][] a){
-		return null;
+	public int[][] adjoint(int[][] matrix){
+		//preconditions
+		if(matrix == null)throw new IllegalArgumentException("matrix must not be null");
+		for(int i = 0; i < matrix.length; i++){
+			if(matrix.length != matrix[i].length)throw new IllegalArgumentException("matrix must be square!");
+		}
+		
+		int[][] newMatrix = new int[matrix.length][matrix.length];
+		
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix.length; j++){
+				newMatrix[i][j] = (int)Math.pow(-1, i+j) * determinant(minor(matrix, i, j));
+			}
+		}
+				
+		return newMatrix;
+	}
+	
+	private int[][] removeRowAndCol(int[][] matrix, int row, int col){
+		//preconditions
+		if(matrix == null)throw new IllegalArgumentException("matrix must not be null");
+		for(int i = 0; i < matrix.length; i++){
+			if(matrix.length != matrix[i].length)throw new IllegalArgumentException("matrix must be square!");
+		}
+		if(matrix.length <= 2)throw new IllegalArgumentException("matrix must be at least 3x3");
+		
+		int[][] newMatrix = new int[matrix.length-1][matrix.length-1];
+		for(int i = 0, ni = 0; i < matrix.length; i++){
+			
+			if(i != row){
+				for(int j = 0, nj = 0; j < matrix.length; j++){
+					if(j != col){
+						newMatrix[ni][nj] = matrix[i][j];
+						nj++;
+					}
+				}
+				ni++;
+			}
+		}
+		return newMatrix;
 	}
 }
